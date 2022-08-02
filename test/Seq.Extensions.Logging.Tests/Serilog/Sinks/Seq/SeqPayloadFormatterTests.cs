9a9c3ce27e1c3f2a7d561d9db459f4fc9e39ddc3
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2022-08-02 14:33:06 +1000

    Basic testing

---
+++ test/Seq.Extensions.Logging.Tests/Serilog/Sinks/Seq/SeqPayloadFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Sinks.Seq;
+using Tests.Support;
+using Xunit;
+
+namespace Seq.Extensions.Logging.Tests.Serilog.Sinks.Seq
+{
+    public class SeqPayloadFormatterTests
+    {
+        [Fact]
+        public void JsonSafeStringPropertiesAreIncludedAsIs()
+        {
+            const string json = "{\"A\": 42}";
+            var evt = Some.LogEvent("The answer is {Answer}", new JsonSafeString(json));
+            var payload = SeqPayloadFormatter.FormatCompactPayload(new[] { evt }, null);
+            Assert.Contains("\"Answer\":{\"A\": 42}", payload);
+        }
+        
+        [Fact]
+        public void DefaultJsonSafeStringsDoNotCorruptPayload()
+        {
+            var evt = Some.LogEvent("The answer is {Answer}", (JsonSafeString)default);
+            var payload = SeqPayloadFormatter.FormatCompactPayload(new[] { evt }, null);
+            Assert.Contains("\"Answer\":\"<null>\"", payload);
+        }
+    }
+}
